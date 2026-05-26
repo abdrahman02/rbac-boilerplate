9d229b118e0b2cb9df2485f170673491c7f7db25
abdrahman <mabdulr44@gmail.com>
2026-05-26 15:16:25 +0700

    feat(common): add DropdownMenuItem

--- frontend/src/shared/components/common/index.ts
+++ frontend/src/shared/components/common/index.ts
@@ -1,4 +1,5 @@
 export { BrandMark } from "./BrandMark";
+export { DropdownMenuItem } from "./DropdownMenuItem";
 export { PageHeader } from "./PageHeader";
 export { RefreshButton } from "./RefreshButton";
 export { SearchInput } from "./SearchInput";
