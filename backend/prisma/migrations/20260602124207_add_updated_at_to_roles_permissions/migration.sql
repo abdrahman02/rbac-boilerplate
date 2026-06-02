03dd0d8e433eb67fc3cfc838e9146042eb17f6ba
abdrahman <mabdulr44@gmail.com>
2026-06-02 19:46:29 +0700

    fix(schema): add DEFAULT CURRENT_TIMESTAMP to updatedAt migration

--- backend/prisma/migrations/20260602124207_add_updated_at_to_roles_permissions/migration.sql
+++ backend/prisma/migrations/20260602124207_add_updated_at_to_roles_permissions/migration.sql
@@ -1,12 +1,5 @@
-/*
-  Warnings:
-
-  - Added the required column `updated_at` to the `permissions` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `updated_at` to the `roles` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
-ALTER TABLE `permissions` ADD COLUMN `updated_at` DATETIME(3) NOT NULL;
+ALTER TABLE `permissions` ADD COLUMN `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
 
 -- AlterTable
-ALTER TABLE `roles` ADD COLUMN `updated_at` DATETIME(3) NOT NULL;
+ALTER TABLE `roles` ADD COLUMN `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
